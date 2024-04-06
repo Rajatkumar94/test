a9b1da6e49c1788487d11458e8e04efc4d1808bb
Rajatkumar94 <rajatkumarrkt@gmail.com>
2024-04-07 00:53:24 +0530

    for testing

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+
+const app = new Hono();
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.post("/signup", async (c) => {
+  try {
+    const prisma = new PrismaClient({
+      datasourceUrl: c.env.DATABASE_URL,
+    }).$use(withAccelerate());
+
+    console.log(c.env.DATABASE_URL);
+
+    const body = await c.req.json();
+
+    console.log(body);
+
+    const newUser = await prisma.user.create({
+      data: {
+        username: body.username,
+        password: body.password,
+      },
+    });
+
+    console.log(newUser);
+
+    return c.json({ success: true });
+  } catch (error) {
+    console.log("Error", error);
+    return c.json({ success: false, error: "Failed to create user" });
+  }
+});
+
+export default app;
